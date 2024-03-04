cb8af26b0416151cfa68af80a6e2c8d9de3d17cc
ANTONIOALGMAR <antonioalgmar@gmail.com>
2024-03-04 17:15:47 -0300

    commit inicial projeto mentoria java

---
+++ src/test/java/jdev/mentoria/loja_virtual/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
